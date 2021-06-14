6bbb092b740ecdcc6a54413638042bb79859fde0
defo <lcd.drm@tutamail.com>
2021-06-14 18:48:14 +0200

    AJOUT TAG ACCUEIL

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 fetch("FishEyeData.json")
 	.then((response) => response.json())
 	.then((data) => {
-		console.log(data);
-
 		//
-
-		const photographers = data.photographers;
-		console.log(photographers);
 
 		//
 
 		data.photographers.forEach((photographer) => {
 			//	DIV PRINCIPALE / CHAQUE ARTISTE
+
+			//
 
 			const photographersBox = document.createElement("div");
 			photographersBox.classList.add("artistBox");
@@ -22,7 +19,6 @@ fetch("FishEyeData.json")
 			const photographersPortrait = document.createElement("img");
 			photographersPortrait.src = `./img/Photographers_ID_Photos/${photographer.portrait}`;
 			photographersPortrait.style.objectFit = "cover";
-			console.log(photographersPortrait);
 			photographersBox.appendChild(photographersPortrait);
 
 			//	P NOM / CHAQUE ARTISTE
@@ -57,13 +53,18 @@ fetch("FishEyeData.json")
 				photographersPrice
 			);
 
-			//
+			// TAG
+			const tagBox = document.createElement("div");
+			photographersPrice.insertAdjacentElement("afterend", tagBox);
+			x = photographer.tags;
 
-			const photographersTag = document.createElement("a");
-			photographersTag.classList.add("artistTag");
-			photographersTag.innerText = photographer.tags;
-			photographersPrice.insertAdjacentElement("afterend", photographersTag);
+			x.forEach(myFunction);
 
-			//
+			function myFunction(item) {
+				const photographersTag = document.createElement("a");
+				photographersTag.classList.add("artistTag");
+				photographersTag.innerText = "#" + "" + item;
+				tagBox.insertAdjacentElement("afterbegin", photographersTag);
+			}
 		});
 	});
